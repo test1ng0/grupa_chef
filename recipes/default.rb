e310f48ae42398a46e9224f9f05cce4ac30460c7
s <s@localhost.localdomain>
2017-11-07 11:15:57 +0200

    versiune stabila de reteta pt. atom

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+#bash 'downloadez atom' do
+#  code <<-AAA
+#    wget https://atom.io/download/rpm -O /tmp/atom.rpm
+#  AAA
+#  not_if "ls /tmp/atom.rpm"
+#end
+remote_file '/tmp/atom.rpm' do
+  source 'https://atom.io/download/rpm'
+#  path '/tmp/atom.rpm'
+# action :create_if_missing
+end
+
+#bash 'instalez atom' do
+#  code <<-AAA
+#    yum install /tmp/atom.rpm
+#  AAA
+#end
+
+package '/tmp/atom.rpm'
+
+
+
+
+
+
